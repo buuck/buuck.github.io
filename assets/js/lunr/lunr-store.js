cacff22710eea94d9a9da6b5fd70cf319e4ef858
Micah Buuck <mbuuck@gmail.com>
2022-10-16 16:31:57 -0700

    Switched to precompiled site

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Uncertainty in Machine Learning",
+        "excerpt":"Experimental physicists spend most of their research time doing 2 things: building their experiment, and trying to figure out how wrong their results could be. You’ll notice that I skipped right over the part where you actually do your experiment and analyze your data. It’s not that those things don’t...","categories": ["Blog"],
+        "tags": ["Physics","Machine Learning"],
+        "url": "/blog/uncertainty-in-machine-learning/",
+        "teaser": null
+      }]
