3ecc5c7e2bdb319fee889c360026ae1d5cf8efe5
Micah Buuck <mbuuck@gmail.com>
2022-11-05 17:40:30 -0700

    Finished first two posts, stable version of front page (for now)

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "Uncertainty in Machine Learning",
-        "excerpt":"Experimental physicists spend most of their research time doing 2 things: building their experiment, and trying to figure out how wrong their results could be. You’ll notice that I skipped right over the part where you actually do your experiment and analyze your data. It’s not that those things don’t...","categories": ["Blog"],
+        "title": "Basic Bayesian Statistics",
+        "excerpt":"Bayes’s Theorem I am an ancestral Minnesota Twins fan, having grown up in Minnesota during the glory days of the M&amp;M boys, Johan Santana, and Ron Gardenhire. Back then, the Twins won a lot of games, just…not during the postseason. In fact, the Twins have never had much success in...","categories": ["Blog"],
+        "tags": ["Statistics","Machine Learning"],
+        "url": "/blog/bayes-theorem/",
+        "teaser": null
+      },{
+        "title": "Predictive Uncertainty with Neural Networks for the GammaTPC Telescope Concept",
+        "excerpt":"Uncertainty in Physics Experimental physicists spend most of their research time doing 2 things: building their experiment, and trying to figure out how wrong their results could be. You’ll notice that I skipped right over the part where you actually do your experiment and analyze your data. It’s not that...","categories": ["Blog"],
         "tags": ["Physics","Machine Learning"],
-        "url": "/blog/uncertainty-in-machine-learning/",
+        "url": "/blog/predictive-uncertainty-gammatpc/",
         "teaser": null
       }]
